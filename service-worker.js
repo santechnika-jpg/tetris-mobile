9dec6e4034891987bc2d711d4552a472b38f4f47
santechnika-jpg <santechnika@gmail.com>
2026-07-06 20:56:47 +0200

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "tetris-mobile-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./game.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
